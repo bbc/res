6d9ac2e3f46cb0e12659dbad2e736a03610cc90a
asim <asimk21@gmail.com>
2015-08-28 10:49:27 +0100

    Added mappings

---
+++ lib/res/mappings.rb
@@ -0,0 +1,23 @@
+module Res
+	class Mappings
+
+		attr_accessor :context, :case, :type
+
+		def initialize(type)
+			@type = type
+			set_type
+		end
+
+	 	def set_type
+      case type
+      when "Junit"
+        @context = ["JUnit::testsuite", "JUnit::testsuites"]
+        @case = ["JUnit::testcase"]      
+      when "Cucumber"
+        @context = ["Cucumber::Feature", "Cucumber::ScenarioOutline"]
+        @case = ["Cucumber::Scenario"]
+      end
+    end
+
+	end
+end
--- lib/res/reporters/test_rail.rb
+++ lib/res/reporters/test_rail.rb
@@ -1,4 +1,6 @@
 require 'test_rail'
+require 'res/ir'
+require 'res/mappings'
 
 module Res
   module Reporters
@@ -8,49 +10,42 @@ module Res
       def initialize(res_file)
         i = 0
         @case_status = {}
-        json = File.read(res_file)
-        @ir = JSON.parse(json, :symbolize_names => true)
         
-        # @io = io
-        # @config = load_config
+        @ir = Res::IR.load(res_file)
+        @mappings = Res::Mappings.new(@ir.type)
 
         project_name = 'bbc-test/post_result'
         suite_name = "Jasmine"
-        project_id = get_project(project_name)
 
+        project_id = get_project(project_name)
         suite_id = get_suite(project_id, suite_name)
-        
-        set_type(@ir[:type]) 
-        
-        while i < @ir[:results].count
+    
+        while i < @ir.results.count         
           # Create a parent section
-          section = @tr.add_section(:project_id => project_id, :suite_id => suite_id, :name => @ir[:results][i][:name], :parent_id => nil) 
-          # Add section or cases within parent section
-          sync_results(@ir[:results][i], project_id, suite_id, section.id)
+          section = @tr.add_section(:project_id => project_id, :suite_id => suite_id, :name => @ir.results[i][:name], :parent_id => nil)   
+          sync_results(@ir.results[i], project_id, suite_id, section.id)
           i += 1
         end # while
-
-        # Add New run
         run_id = add_test_run(project_id, suite_id)
-
-        # Add status to each testcase
         add_case_status(run_id)
       end # initialize
 
       def tr
-        @tr = TestRail::API.new( :user  => "username",
-                                 :password  => "password",
-                                 :namespace => "namespace" )
+        @tr = TestRail::API.new( :user  => "testrail@example.com",
+                                 :password  => "g0ne8ang",
+                                 :namespace => "bbcsandbox" )
       end
 
+      # Add status to each testcase
       def add_case_status(run_id)
         @case_status.each { |case_id, status|
           @tr.add_result_for_case(:run_id => run_id, :case_id => case_id, :status => status)
         }
       end
 
+      # Add New Test run
       def add_test_run(project_id, suite_id)
-        run = @tr.add_run(:project_id => project_id, :suite_id => suite_id, :description => "#{@ir[:type]}_#{Time.now}")
+        run = @tr.add_run(:project_id => project_id, :suite_id => suite_id, :description => "#{@ir.type}_#{Time.now}")
         run.id
       end
 
@@ -67,29 +62,17 @@ module Res
             return suite.id
           end
         end
-        suite = @tr.add_suite(:project_id => project_id, :name => "#{@ir[:type]}_Suite")
+        suite = @tr.add_suite(:project_id => project_id, :name => "#{@ir.type}_Suite")
         suite.id
       end
 
-      # Set types to be included in context and case
-      def set_type(type)
-        case type
-        when "Junit"
-          @context = ["JUnit::testsuite", "JUnit::testsuites"]
-          @case = ["JUnit::testcase"]      
-        when "Cucumber"
-          @context = ["Cucumber::Feature", "Cucumber::ScenarioOutline"]
-          @case = ["Cucumber::Scenario"]
-        end
-      end
-
-      # Add sections or cases 
+      # Add section or cases within parent section
       def sync_results(ir, project_id, suite_id, parent_id)
         ir[:children].each do |child|
-          if @context.include?(child[:type])
+          if @mappings.context.include?(child[:type])
             section = @tr.add_section(:project_id => project_id, :suite_id => suite_id, :name => child[:name], :parent_id => parent_id) 
             sync_results(child, project_id, suite_id, section.id) if child[:children].count > 0
-          elsif @case.include?(child[:type])
+          elsif @mappings.case.include?(child[:type])
             parent_id = suite_id if parent_id == nil  
             tcase = @tr.add_case(:section_id => parent_id, :title => child[:name])
             @case_status[:"#{tcase.id}"] = child[:status]
