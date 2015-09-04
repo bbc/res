2035741ec1b46a7ea0a9f69ce750fff6d0dbe4e1
asim <asimk21@gmail.com>
2015-09-04 16:11:28 +0100

    Test Rail updated

--- lib/res/reporters/test_rail.rb
+++ lib/res/reporters/test_rail.rb
@@ -1,39 +1,55 @@
 require 'test_rail'
 require 'res/ir'
 require 'res/mappings'
-
+require 'pry'
 module Res
   module Reporters
-    class TestRailR
+    class TestRail
 
       attr_accessor :ir, :case_status
       def initialize(res_file)
-        i = 0
         @case_status = {}
         
         @ir = Res::IR.load(res_file)
         @mappings = Res::Mappings.new(@ir.type)
-
         project_name = 'bbc-test/post_result'
-        suite_name = "Jasmine"
+        @suite_name = "Jasmine"
 
-        project_id = get_project(project_name)
-        suite_id = get_suite(project_id, suite_name)
-    
-        while i < @ir.results.count         
-          # Create a parent section
-          section = @tr.add_section(:project_id => project_id, :suite_id => suite_id, :name => @ir.results[i][:name], :parent_id => nil)   
-          sync_results(@ir.results[i], project_id, suite_id, section.id)
-          i += 1
-        end # while
-        run_id = add_test_run(project_id, suite_id)
-        add_case_status(run_id)
+        @project = tr.find_project(:name => project_name)
+        raise "Couldn't find project with name #{project_name}" if @project.nil?
       end # initialize
 
+
+      def sync_tests(args = {})
+        suite = @project.find_or_create_suite(:name => @suite_name, :id => @project.id)
+        i = 0
+          while i < @ir.results.count         
+            section = suite.find_or_create_section(:project_id => @project.id, :suite_id => suite.id, :name => @ir.results[i][:name])   
+            sync_results(@ir.results[i], @project.id, suite, section)
+            i += 1
+          end # while
+      end
+
+      def submit_results(args)
+        suite = @project.find_suite(:name => @suite_name)
+        raise "Couldn't find suite with name #{@suite_name}" if suite.nil?
+        run_id = add_test_run(@project.id, suite.id)
+        i = 0
+        if !@case_status.empty?
+          while i < @ir.results.count
+            section = suite.find_section(:name => @ir.results[i][:name])
+            raise "Couldn't find section with name #{@ir.results[i][:name]}" if section.nil?
+            case_details(@ir.results[i], section)
+            i += 1
+          end # while
+        end # if
+        add_case_status(run_id)
+      end
+
       def tr
-        @tr = TestRail::API.new( :user  => "user",
-                                 :password  => "password",
-                                 :namespace => "namespace" )
+        @tr = ::TestRail::API.new( :user => "user",
+                                  :password => "password",
+                                  :namespace => "namespace" )
       end
 
       # Add status to each testcase
@@ -45,42 +61,38 @@ module Res
 
       # Add New Test run
       def add_test_run(project_id, suite_id)
-        run = @tr.add_run(:project_id => project_id, :suite_id => suite_id, :description => "#{@ir.type}_#{Time.now}")
+        run = @tr.add_run(:project_id => project_id, :suite_id => suite_id, :name => "run_name", :description => "#{@ir.type}_#{Time.now}")
         run.id
       end
 
-      def get_project(project_name)
-        project = @tr.find_project(:name => project_name)
-        project.id
-      end
-
-      # Returns the newly created or existing suite id
-      def get_suite(project_id, suite_name)
-        suites = @tr.get_suites(:project_id => project_id)
-        suites.each do |suite|
-          if suite.name == suite_name
-            return suite.id
-          end
-        end
-        suite = @tr.add_suite(:project_id => project_id, :name => "#{@ir.type}_Suite")
-        suite.id
-      end
-
       # Add section or cases within parent section
-      def sync_results(ir, project_id, suite_id, parent_id)
+      def sync_results(ir, project_id, suite, parent)
         ir[:children].each do |child|
           if @mappings.context.include?(child[:type])
-            section = @tr.add_section(:project_id => project_id, :suite_id => suite_id, :name => child[:name], :parent_id => parent_id) 
-            sync_results(child, project_id, suite_id, section.id) if child[:children].count > 0
+            section = parent.find_or_create_section(:project_id => project_id, :suite_id => suite.id, :name => child[:name])   
+            sync_results(child, project_id, suite, section) if child[:children].count > 0
           elsif @mappings.case.include?(child[:type])
-            parent_id = suite_id if parent_id == nil  
-            tcase = @tr.add_case(:section_id => parent_id, :title => child[:name])
+            parent = suite if parent == nil  
+            tcase = parent.find_or_create_test_case(:title => child[:name])
             @case_status[:"#{tcase.id}"] = child[:status]
           else
             # To be added. Ex: steps 
           end # if
         end # each
       end # sync_results
+
+
+      def case_details(ir, section)
+        ir[:children].each do |child|
+          if @mappings.context.include?(child[:type])
+              section = section.find_section(:name => child[:name])   
+              case_details(child, section)
+          elsif @mappings.case.include?(child[:type])  
+              tcase = section.find_test_case(:title => child[:name]) if !section.nil? 
+              @case_status[:"#{tcase.id}"] = child[:status] if !tcase.nil?
+          end
+        end
+      end
 
     end # TestRail
   end # Reporters
