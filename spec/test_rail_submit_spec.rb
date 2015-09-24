b1ba0f6aea8c6de330c0345378d16a30ef7837ff
asim <asimk21@gmail.com>
2015-09-24 15:26:11 +0100

    Added spec

--- spec/test_rail_submit_spec.rb
+++ spec/test_rail_submit_spec.rb
@@ -9,12 +9,29 @@ describe Res::Reporters::TestRail do
       
       junit_command = "RUBYLIB=lib bundle exec ./bin/res.rb --junit './examples/junit/junittests.xml' --submit testrail --config-file ./examples/cucumber/.test_rail.yaml"
       
-      before(:all) do
+      before(:each) do
         @output = `#{junit_command}`
       end
 
       it "Submit the test results" do
         expect( @output ).to match /Submitted to Test Rail/
+      end
+
+    end
+  end
+
+  describe "Submitting Cucumber results in testrail" do
+    describe "Create a new test run and submit results" do
+      
+      cucumber_command = "unset BUNDLE_GEMFILE; unset RUBYOPT; cd examples/cucumber; bundle install --without production test > /dev/null && VERSION=12 TARGET=nothing bundle exec cucumber"
+      
+      before(:each) do
+        `#{cucumber_command} features/tags.feature -f Res::Formatters::RubyCucumber -o ./cucumber.res`
+        @output = `RUBYLIB=lib ./bin/res.rb --res examples/cucumber/cucumber.res --submit testrail --config-file ./examples/cucumber/.test_rail.yaml`
+      end
+
+      it "Submit the test results" do
+         expect( @output ).to include ("Submitted to Test Rail")
       end
 
     end
