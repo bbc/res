91ceedd422c58fa8417a9127239815db436951c2
asim <asimk21@gmail.com>
2015-09-25 14:22:05 +0100

    Added skip if .test_rail.yaml not found

--- spec/test_rail_submit_spec.rb
+++ spec/test_rail_submit_spec.rb
@@ -3,17 +3,35 @@ require 'res/reporters/test_rail'
 require 'json'
 
 describe Res::Reporters::TestRail do
+
+  before(:all) do
+    if File.exists?("./examples/cucumber/.test_rail.yaml")
+      @exists = true
+    else
+      @exists = false
+    end
+  end
   
   describe "Submitting Junit results in testrail" do
     describe "Create a new test run and submit results" do
       
-      junit_command = "RUBYLIB=lib bundle exec ./bin/res.rb --junit './examples/junit/junittests.xml' --submit testrail --config-file ./examples/cucumber/.test_rail.yaml"
+      junit_command = "RUBYLIB=lib bundle exec ruby ./bin/res.rb --junit './examples/junit/junittests.xml' --submit testrail --config-file ./examples/cucumber/.test_rail.yaml"
       
-      before(:each) do
-        @output = `#{junit_command}`
+      it "Skipping tests", :if => !@exists do
+        puts "Create file ./examples/cucumber/.test_rail.yaml with below template
+              namespace: 'namespace'
+              url:       'url'
+              user:      'username'
+              password:  'password'
+              suite:     'SuiteName'
+              project:   'ProjectName'
+              run_name:  'RunName'
+              run_id:    'run_id'"
+        puts "Skipping Rspec tests for test rail as .test_rail.yaml not found"
       end
 
-      it "Submit the test results" do
+      it "Submit the test results", :if => @exists do
+        @output = `#{junit_command}`
         expect( @output ).to match /Submitted to Test Rail/
       end
 
@@ -24,14 +42,11 @@ describe Res::Reporters::TestRail do
     describe "Create a new test run and submit results" do
       
       cucumber_command = "unset BUNDLE_GEMFILE; unset RUBYOPT; cd examples/cucumber; bundle install --without production test > /dev/null && VERSION=12 TARGET=nothing bundle exec cucumber"
-      
-      before(:each) do
-        `#{cucumber_command} features/tags.feature -f Res::Formatters::RubyCucumber -o ./cucumber.res`
-        @output = `RUBYLIB=lib ./bin/res.rb --res examples/cucumber/cucumber.res --submit testrail --config-file ./examples/cucumber/.test_rail.yaml`
-      end
 
-      it "Submit the test results" do
-         expect( @output ).to include ("Submitted to Test Rail")
+      it "Submit the test results", :if => @exists do
+        `#{cucumber_command} features/tags.feature -f Res::Formatters::RubyCucumber -o ./cucumber.res`
+        @output = `RUBYLIB=lib bundle exec ruby ./bin/res.rb --res examples/cucumber/cucumber.res --submit testrail --config-file ./examples/cucumber/.test_rail.yaml`
+        expect( @output ).to include ("Submitted to Test Rail")
       end
 
     end
