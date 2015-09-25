a6147cb1e2f08d10fc3b5b45ed98fbcc6d51b62c
asim <asimk21@gmail.com>
2015-09-25 16:06:32 +0100

    Skip tests

--- spec/test_rail_submit_spec.rb
+++ spec/test_rail_submit_spec.rb
@@ -3,21 +3,13 @@ require 'res/reporters/test_rail'
 require 'json'
 
 describe Res::Reporters::TestRail do
-
-  before(:all) do
-    if File.exists?("./examples/cucumber/.test_rail.yaml")
-      @exists = true
-    else
-      @exists = false
-    end
-  end
-  
+ 
   describe "Submitting Junit results in testrail" do
     describe "Create a new test run and submit results" do
       
       junit_command = "RUBYLIB=lib bundle exec ruby ./bin/res.rb --junit './examples/junit/junittests.xml' --submit testrail --config-file ./examples/cucumber/.test_rail.yaml"
       
-      it "Skipping tests", :if => !@exists do
+      it "Skipping tests", :if => !File.exists?("./examples/cucumber/.test_rail.yaml") do
         puts "Create file ./examples/cucumber/.test_rail.yaml with below template
               namespace: 'namespace'
               url:       'url'
@@ -30,7 +22,7 @@ describe Res::Reporters::TestRail do
         puts "Skipping Rspec tests for test rail as .test_rail.yaml not found"
       end
 
-      it "Submit the test results", :if => @exists do
+      it "Submit the test results", :if => File.exists?("./examples/cucumber/.test_rail.yaml") do
         @output = `#{junit_command}`
         expect( @output ).to match /Submitted to Test Rail/
       end
@@ -43,7 +35,7 @@ describe Res::Reporters::TestRail do
       
       cucumber_command = "unset BUNDLE_GEMFILE; unset RUBYOPT; cd examples/cucumber; bundle install --without production test > /dev/null && VERSION=12 TARGET=nothing bundle exec cucumber"
 
-      it "Submit the test results", :if => @exists do
+      it "Submit the test results", :if => File.exists?("./examples/cucumber/.test_rail.yaml") do
         `#{cucumber_command} features/tags.feature -f Res::Formatters::RubyCucumber -o ./cucumber.res`
         @output = `RUBYLIB=lib bundle exec ruby ./bin/res.rb --res examples/cucumber/cucumber.res --submit testrail --config-file ./examples/cucumber/.test_rail.yaml`
         expect( @output ).to include ("Submitted to Test Rail")
