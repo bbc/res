ca4c9b6e125a83e7e5a5c389d2c50ee5e65cd08a
asim <asimk21@gmail.com>
2015-11-13 11:18:57 +0000

    Added expect to check if added value is hash

--- spec/performance_spec.rb
+++ spec/performance_spec.rb
@@ -6,5 +6,6 @@ describe Res.perf_data do
         `#{cucumber_command} features/performance.feature -f Res::Formatters::RubyCucumberLegacy -o out.res`
         ir = Res::IR.load("examples/cucumber/out.res")
         expect(ir.tests.first).to have_key(:value)
+        expect(ir.tests.first[:value]).to be_a Hash
       end
 end
