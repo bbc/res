5a87e63e3da17d93d1afdb6205a4ff77c757871a
Joe Haig <joe.haig@bbc.co.uk>
2016-05-16 12:06:32 +0100

    Avoid empty values fields

--- lib/res/formatters/rspec.rb
+++ lib/res/formatters/rspec.rb
@@ -109,11 +109,13 @@ module Res
       def set_status(result, urn, status)
         result.each do |r|
           if r[:type] == "Rspec::Describe"
-            r[:values] ||= {}
-            Res.perf_data.each do |p|
-              r[:values].merge! p
+            if Res.perf_data.size > 0
+              r[:values] ||= {}
+              Res.perf_data.each do |p|
+                r[:values].merge! p
+              end
+              Res.perf_data = []
             end
-            Res.perf_data = []
             set_status(r[:children], urn, status)
           elsif r[:type] == "Rspec::Test"
             if r[:urn] == urn
