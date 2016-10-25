9af0b29506ad93d55a884d0b805274c3754b2b17
jonpwilson <jon.wilson01@bbc.co.uk>
2016-10-25 10:25:02 +0100

    Added the erroring test check

--- spec/res/parsers/android_junit_spec.rb
+++ spec/res/parsers/android_junit_spec.rb
@@ -4,6 +4,7 @@ require 'json'
 describe Res::Parsers::AndroidJunit do
   let(:pass_test) { 'spec/outputs/android_junit_single_pass.out' }
   let(:failing_test) { 'spec/outputs/android_junit_single_fail.out' }
+  let(:erroring_test) { 'spec/outputs/android_junit_single_error_with_retry.out'}
 
   before(:each) do
     File.delete('instruments.res') if File.exist?('instruments.res')
@@ -25,6 +26,11 @@ describe Res::Parsers::AndroidJunit do
       expect(ir.count(:failed)).to eq 1
       expect(ir.count(:passed)).to eq 0
       expect(ir.count(:unknown)).to eq 0
+    end
+
+    it 'returns the correct results when parsing an erroring test' do
+      Res::Parsers::AndroidJunit.new(erroring_test)
+      ir = Res::IR.load('instruments.res')
     end
 
   end
